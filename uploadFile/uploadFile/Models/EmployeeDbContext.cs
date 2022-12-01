0ad53f77c5341bba6244998648ab6dc11314c25d
hieu <hieuvnautotech@gmail.com>
2022-12-01 16:44:21 +0700

    cty

---
+++ uploadFile/uploadFile/Models/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace uploadFile.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options):base(options) {
+                
+        }
+
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
---
+++ uploadFile/uploadFile/Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.NetworkInformation;
+
+namespace uploadFile.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string EmployeeName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Occupation { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
+    }
+}
